32390c826c65709abe7b65a2231b7f04d2bedcd7
brenoguimaraes <brenoguimaraes@mpto.mp.br>
2024-04-05 14:58:06 -0300

    acessando estruturas de dados por html

--- autentication/views.py
+++ autentication/views.py
@@ -5,12 +5,20 @@ def cadastro(request):
     nome = "Breno"
     idade = 22
     profissao = 'garoto de programa'
-    atributos_cadastro = {
+    atributos_cadastro = [{
         "nome" : nome,
         "idade" : idade,
         "profissao" : profissao,
-    }
-    return render(request,"cadastro.html",{"nome":nome,"idade":idade,"profissao":profissao})
+    },{
+        "nome" : "Lucas",
+        "idade" : "23",
+        "profissao" : "Vendedor",
+    }]
+
+    # Para renderizar os valores no HTML você faz assim:
+    # atributos_cadastro.nome da variavel -> CERTO
+    # atributos_cadastro[nome da variavel] -> ERRADO
+    return render(request,"cadastro.html",{"atributos_cadastro":atributos_cadastro})
 
 def home(request):
     return HttpResponse(content=F"Home Page Base Auth Controller")
