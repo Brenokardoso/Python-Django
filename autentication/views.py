f4e6fb652fdbc7c9a2d6fd7d2878c6f0362fb1ab
brenoguimaraes <brenoguimaraes@mpto.mp.br>
2024-04-08 10:24:42 -0300

    uso do block para alteração dos arquivos básicos estáticos

--- autentication/views.py
+++ autentication/views.py
@@ -1,30 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def cadastro(request):
-    nome = "Breno"
-    idade = 22
-    profissao = 'garoto de programa'
-    atributos_cadastro = [{
-        "nome" : nome,
-        "idade" : idade,
-        "profissao" : profissao,
-    },{
-        "nome" : "Lucas",
-        "idade" : "23",
-        "profissao" : "Vendedor",
-    },
-    {
-        "validator" : True
-    }]
-    lista_de_compras = ["abacaxi","pera","maça"]
-
-    # Para renderizar os valores no HTML você faz assim:
-    # atributos_cadastro.nome da variavel -> CERTO
-    # atributos_cadastro[nome da variavel] -> ERRADO
-
-    return render(request,"cadastro.html",{"atributos_cadastro":atributos_cadastro,
-                                            "compras" : lista_de_compras})
-
 def home(request):
     return HttpResponse(content=F"Home Page Base Auth Controller")
+
+def cadastro(request):
+    return render(request,'cadastro.html')
+
+
+
+
+# def cadastro(request):
+#     nome = "Breno"
+#     idade = 22
+#     profissao = 'garoto de programa'
+#     atributos_cadastro = [{
+#         "nome" : nome,
+#         "idade" : idade,
+#         "profissao" : profissao,
+#     },{
+#         "nome" : "Lucas",
+#         "idade" : "23",
+#         "profissao" : "Vendedor",
+#     },
+#     {
+#         "validator" : True
+#     }]
+#     lista_de_compras = ["abacaxi","pera","maça"]
+
+#     # Para renderizar os valores no HTML você faz assim:
+#     # atributos_cadastro.nome da variavel -> CERTO
+#     # atributos_cadastro[nome da variavel] -> ERRADO
+
+#     return render(request,"cadastro.html",{"atributos_cadastro":atributos_cadastro,
+#                                             "compras" : lista_de_compras})
