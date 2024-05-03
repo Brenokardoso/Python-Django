80f0c755f936892ca64f905a7aa1f29c13421787
brenoguimaraes <brenoguimaraes@mpto.mp.br>
2024-05-03 09:48:36 -0300

    usando makemigrations e migrates

--- autentication/models.py
+++ autentication/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(
+        verbose_name = "Nome",
+        null = True,
+        blank= True,
+        max_length = 100,
+    )
+    email = models.EmailField(
+        verbose_name = "Email",
+        null = True,
+        blank = True,
+    )
+    senha = models.CharField(
+        verbose_name = "Senha",
+        null = True,
+        blank = True,
+        max_length = 100, 
+    )
