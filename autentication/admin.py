459854f7b25a20162c774a8e3ac62d7de74eb72e
brenoguimaraes <brenoguimaraes@mpto.mp.br>
2024-06-17 15:31:19 -0300

    django do básico até o djangoa admin

--- autentication/admin.py
+++ autentication/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Pessoa, Cargos, Pedido
+from autentication.models import Pessoa, Cargos, Pedido
 from django_object_actions import DjangoObjectActions
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # admin.site.register(Pessoa) // Registrando no modulo de admin o Modelo de Pessoa
 # admin.site.register(Cargos) // Registrando no módulo de admin o Modelo de Cargos
@@ -25,9 +26,15 @@ class PessoaAdmin(DjangoObjectActions, admin.ModelAdmin):
     def mostra_pessoa(self, request, pessoa):
         return HttpResponse(f"tome a request")
 
+    def lista_pessoa(self, request, people):
+        pessoa = Pessoa.objects.filter(nome=people.nome)
+        print(f"valor do params pessoa : {people}")
+        return render(request, "listagem.html", context={"pessoas": pessoa})
+
+    lista_pessoa.label = "Lista"
     mostra_pessoa.label = "mostrar pessoa"
 
-    change_actions = ("mostra_pessoa",)
+    change_actions = ("mostra_pessoa", "lista_pessoa")
 
 
 @admin.register(Cargos)
